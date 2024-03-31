0ebac696e7bc77c29271793011eb1a0f09164c82
jefeez <jefersonquez@gmail.com>
2024-03-31 20:15:32 -0300

    mult envs

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,7 @@
 import { z } from 'zod'
 
 const schemas = z.object({
-  NODE_ENV: z.string().default('development'),
-  PORT: z.string().transform((port) => Number(port)),
+  NODE_ENV: z.string().default('dev'),
 })
 
 export const env = schemas.parse(process.env)
